978ebbedb1ba9dd70417568823a43ada4094d151
Tommy Oh <tommyhyungjinoh@gmail.com>
2018-08-30 23:40:26 -0700

    page 60 exercise started.

---
+++ 01_The_Dojo_Collection/01_Fundamentals/page_60.js
@@ -0,0 +1,15 @@
+"use strict";
+
+// SList: Length
+
+// SList: Max
+console.log("----------------------------------------");
+
+// SList: Display
+console.log("----------------------------------------");
+
+// SList: Min
+console.log("----------------------------------------");
+
+// SList: Average
+console.log("----------------------------------------");
