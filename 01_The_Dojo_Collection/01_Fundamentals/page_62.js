a15923e4653f7b5e1b5dd7937348485ed66cd114
Tommy Oh <tommyhyungjinoh@gmail.com>
2018-09-02 22:43:35 -0700

    page 62 exercise has been started.

---
+++ 01_The_Dojo_Collection/01_Fundamentals/page_62.js
@@ -0,0 +1,13 @@
+"use strict";
+
+// Create SList (prompt)
+console.log('----------------------------------')
+
+// SList: Prepend Val
+console.log('----------------------------------')
+
+// SList: Remove Val
+console.log('----------------------------------')
+
+// SList: Append Val
+console.log('----------------------------------')
