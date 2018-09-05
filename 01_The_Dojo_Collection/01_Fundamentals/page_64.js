91d8e66b468e28dc3e76566e24ac49f458015a2b
Tommy Oh <tommyhyungjinoh@gmail.com>
2018-09-04 23:47:44 -0700

    page 64 exercise has been started.

---
+++ 01_The_Dojo_Collection/01_Fundamentals/page_64.js
@@ -0,0 +1,13 @@
+"use strict"
+
+// SList: Second to Last Value
+console.log('------------------------------------')
+
+// SList: Copy
+console.log('------------------------------------')
+
+// SList: Delete Given Node
+console.log('------------------------------------')
+
+// SList: Filter
+console.log('------------------------------------')
