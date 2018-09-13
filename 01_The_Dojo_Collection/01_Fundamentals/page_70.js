5bbe51988f74303b33013e217a201837207d9cc7
Tommy Oh <tommyhyungjinoh@gmail.com>
2018-09-12 23:17:36 -0700

    page 70 exercise has been started.

---
+++ 01_The_Dojo_Collection/01_Fundamentals/page_70.js
@@ -0,0 +1,20 @@
+"use strict";
+
+// SLQueue: Enqueue
+
+// SLQueue: Dequeue
+
+// SLQueue: Front
+
+// SLQueue: Contains
+
+// SLQueue: Is Empty
+
+// SLQueueL Size
+
+// SLQueue: Compare Queues
+
+// SLQueue: Remove Min
+
+// SLQueue: Interleave Queue
+
