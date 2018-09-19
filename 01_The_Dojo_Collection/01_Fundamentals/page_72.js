bb6f21049060c7862ad1956837fa0da8585d0bd6
Tommy Oh <tommyhyungjinoh@gmail.com>
2018-09-18 23:58:56 -0700

    page 72 exercise has been started.

---
+++ 01_The_Dojo_Collection/01_Fundamentals/page_72.js
@@ -0,0 +1,19 @@
+"use strict";
+
+console.log("----------------------------------");
+// Stack: Copy
+
+console.log("----------------------------------");
+// Create Queue Using Two Stacks
+
+console.log("----------------------------------");
+// Queue: Is Palindrome
+
+console.log("----------------------------------");
+// Stack / Queue Code-Sharing
+
+console.log("----------------------------------");
+// Deque: Implementation
+
+console.log("----------------------------------");
+// Stack: Remove Stack Min
