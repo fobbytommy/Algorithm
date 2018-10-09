1892d300a8724c1faddce14cad9d450c8940a64d
Tommy Oh <tommyhyungjinoh@gmail.com>
2018-10-08 21:14:56 -0700

    bubble sort has been completed.

--- 02_sorting/02_bubbleSort.js
+++ 02_sorting/02_bubbleSort.js
@@ -1,3 +1,32 @@
 /*
     Bubble Sort
 */
+
+// Random number generator
+const arr = [];
+const num = 10;
+for (let i = 0; i < num; i++) {
+	let ranNum = Math.round(Math.random() * 50);
+	arr.push(ranNum);
+}
+// const arr = [5, 1, 4, 2, 8];
+
+// log before sorting
+console.log(arr);
+
+const bubbleSort = arr => {
+	let currentLength = arr.length;
+	while (currentLength) {
+		for (let i = 0; i < currentLength - 1; i++) {
+			if (arr[i] > arr[i + 1]) {
+				// ES6 swap
+				[arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+			}
+		}
+		// console.log(arr);
+		currentLength--;
+	}
+};
+
+bubbleSort(arr);
+console.log(arr);
