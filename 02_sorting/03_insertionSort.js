3298eba2607d2e8dc5a8b4d6b04da48369a15b6e
Tommy Oh <tommyhyungjinoh@gmail.com>
2018-10-09 23:56:59 -0700

    insertion sort exercise has been started.

---
+++ 02_sorting/03_insertionSort.js
@@ -0,0 +1,14 @@
+/*
+    Insertion Sort
+*/
+
+// Random number generator
+// const arr = [];
+// const num = 10;
+// for (let i = 0; i < num; i++) {
+// 	let ranNum = Math.round(Math.random() * 50);
+// 	arr.push(ranNum);
+// }
+const arr = [5, 1, 4, 2, 8];
+
+const insertionSort = arr => {};
