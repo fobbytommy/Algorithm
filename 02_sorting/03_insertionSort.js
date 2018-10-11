4ea2ebf37e86e6efebfc90890a1c911453cf5836
Tommy Oh <tommyhyungjinoh@gmail.com>
2018-10-10 23:32:28 -0700

    insertion sort has been completed.

--- 02_sorting/03_insertionSort.js
+++ 02_sorting/03_insertionSort.js
@@ -3,12 +3,26 @@
 */
 
 // Random number generator
-// const arr = [];
-// const num = 10;
-// for (let i = 0; i < num; i++) {
-// 	let ranNum = Math.round(Math.random() * 50);
-// 	arr.push(ranNum);
-// }
-const arr = [5, 1, 4, 2, 8];
+const arr = [];
+const num = 10;
+for (let i = 0; i < num; i++) {
+	let ranNum = Math.round(Math.random() * 50);
+	arr.push(ranNum);
+}
+// const arr = [6, 5, 3, 1, 8, 7, 2, 4];
 
-const insertionSort = arr => {};
+console.log(arr);
+
+const insertionSort = arr => {
+	for (let i = 1; i < arr.length; i++) {
+		for (let j = 0; j < i; j++) {
+			if (arr[j] > arr[i]) {
+				[arr[j], arr[i]] = [arr[i], arr[j]];
+			}
+		}
+		// console.log(arr);
+	}
+};
+
+insertionSort(arr);
+console.log(arr);
