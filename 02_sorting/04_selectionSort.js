255a86646d04b050eda33a401b208c4fd76e360d
Tommy Oh <tommyhyungjinoh@gmail.com>
2018-10-11 23:59:54 -0700

    selection sort exercise has been started.

---
+++ 02_sorting/04_selectionSort.js
@@ -0,0 +1,14 @@
+/*
+    Selection Sort
+*/
+
+// Random number generator
+const arr = [];
+const num = 10;
+for (let i = 0; i < num; i++) {
+	let ranNum = Math.round(Math.random() * 50);
+	arr.push(ranNum);
+}
+// const arr = [6, 5, 3, 1, 8, 7, 2, 4];
+
+console.log(arr);
