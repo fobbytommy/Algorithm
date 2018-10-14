50ccf39933ce8976159755bb66fd90bbc73fb798
Tommy Oh <tommyhyungjinoh@gmail.com>
2018-10-14 16:23:56 -0700

    selection sort exercise has been completed.

--- 02_sorting/04_selectionSort.js
+++ 02_sorting/04_selectionSort.js
@@ -10,5 +10,22 @@ for (let i = 0; i < num; i++) {
 	arr.push(ranNum);
 }
 // const arr = [6, 5, 3, 1, 8, 7, 2, 4];
+console.log(arr);
 
+const selectionSort = arr => {
+	for (let i = 0; i < arr.length; i++) {
+		let minIdx = i;
+		for (let j = i + 1; j < arr.length; j++) {
+			if (arr[j] < arr[minIdx]) {
+				minIdx = j;
+			}
+		}
+		if (i !== minIdx) {
+			[arr[i], arr[minIdx]] = [arr[minIdx], arr[i]];
+		}
+		// console.log(arr);
+	}
+};
+
+selectionSort(arr);
 console.log(arr);
